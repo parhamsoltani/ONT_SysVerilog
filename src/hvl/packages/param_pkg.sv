87a6c74d35fca69de709f44db9b03e486739d9bb
Taha Memari <t.memari81.b@gmail.com>
2024-08-31 02:26:53 +0330

    add CSV params to param_pkg.sv

---
+++ src/hvl/packages/param_pkg.sv
@@ -0,0 +1,23 @@
+package param_pkg;
+
+    // global params & typedefs 
+
+
+
+    // C4 params & typedefs    
+    parameter c4_Lenght = 260,
+              c4_Width = 9, 
+              Byte_Num = 8;
+
+    typedef int c4_index_fixed_array2_t[2];
+
+
+
+    // CSV params & typedefs
+    parameter row_based = 0,
+              col_based = 0;
+
+    typedef int dimensions_t[2];
+
+
+endpackage
