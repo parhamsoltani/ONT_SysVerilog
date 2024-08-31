0dfc6668b0235cd657c402f8d622a719a9afd142
alisoltani8200 <110608793+alisoltani8200@users.noreply.github.com>
2024-08-31 18:18:22 +0330

    Add files via upload

--- src-hvl/classes/STM-1.sv
+++ src-hvl/classes/STM-1.sv
@@ -3,13 +3,14 @@ import class_pkg::*;
 
 class STM_1;
     AU4 au4;
-    bit [STM1_Lenght*STM1_Width*Byte_Num-1:0] frame;
+    bit [STM1_Width-1:0][STM1_Lenght-1:0][Byte_Num-1:0] frame;
     bit [7:0] A1, A2, C1, J0, B1, E1, F1, D1, D2, D3;
     bit [7:0] K1, K2, D4, D5, D6, D7, D8, D9, D10, D11, D12, S1, Z1, Z2, M1, E2;
     bit [23:0] B2;
     bit [7:0] previous_B1;
     bit [23:0] previous_B2;
-    
+
+
     static bit [15:0][7:0] previous_J0_frame;
     static bit first = 1;
     static bit [3:0] counter_for_J0_frame = 0;
@@ -36,6 +37,34 @@ class STM_1;
             counter_for_J0_frame++;
         end
         
+
+        A1 = calculate_A1();
+        A2 = calculate_A2();
+        C1 = calculate_C1();
+        J0 = calculate_J0();
+        B1 = calculate_B1();
+        E1 = calculate_E1();
+        F1 = calculate_F1();
+        D1 = calculate_D1();
+        D2 = calculate_D2();
+        D3 = calculate_D3();
+
+        K1 = calculate_K1();
+        K2 = calculate_K2();
+        D4 = calculate_D4();
+        D5 = calculate_D5();
+        D6 = calculate_D6();
+        D7 = calculate_D7();
+        D8 = calculate_D8();
+        D9 = calculate_D9();
+        D10 = calculate_D10();
+        D11 = calculate_D11();
+        D12 = calculate_D12();
+        S1 = calculate_S1();
+        Z1 = calculate_Z1();
+        Z2 = calculate_Z2();
+        M1 = calculate_M1();
+        E2 = calculate_E2();        
     endfunction
 
     function void pre_randomize();
@@ -55,130 +84,130 @@ class STM_1;
 
     endfunction
     
-    function bit [STM1_Lenght*STM1_Width*Byte_Num-1:0] get_stm_frame();
-        bit [STM1_Lenght*STM1_Width*Byte_Num-1:0] frame;
-        for (int i = 0; i < STM1_Lenght*STM1_Width*Byte_Num ; i++) begin
-            int row = i / STM1_Lenght;
-            int col = i % STM1_Lenght;
-
-            if(col < 9) begin
-                case (col)
-                    0 : case (row)
-                        0 : frame[i] = 0;
-                        1 : frame[i] = 0;
-                        2 : frame[i] = 0;
-                        3 : frame[i] = 0;
-                        4 : frame[i] = 0;
-                        5 : frame[i] = 0;
-                        6 : frame[i] = 0;
-                        7 : frame[i] = 0;
-                        8 : frame[i] = 0;
-                        default: frame[i] = 0;
+    function bit [STM1_Width-1:0][STM1_Lenght-1:0][Byte_Num-1:0]  get_stm_frame();
+        bit [STM1_Width-1:0][STM1_Lenght-1:0][Byte_Num-1:0]  frame;
+        for (int i = 0; i < STM1_Width ; i++) begin
+            for (int j = 0; j < STM1_Lenght ; j++) begin
+                if(j < 9) begin
+                case (j)
+                    0 : case (i)
+                        0 : frame[i][j] = A1;
+                        1 : frame[i][j] = A1;
+                        2 : frame[i][j] = A1;
+                        3 : frame[i][j] = A2;
+                        4 : frame[i][j] = A2;
+                        5 : frame[i][j] = A2;
+                        6 : frame[i][j] = J0;
+                        7 : frame[i][j] = 8'b0;
+                        8 : frame[i][j] = 8'b0;
+                        default: frame[i][j] = 8'b0;
                     endcase
-                    1 : case (row)
-                        0 : frame[i] = 0;
-                        1 : frame[i] = 0;
-                        2 : frame[i] = 0;
-                        3 : frame[i] = 0;
-                        4 : frame[i] = 0;
-                        5 : frame[i] = 0;
-                        6 : frame[i] = 0;
-                        7 : frame[i] = 0;
-                        8 : frame[i] = 0;
-                        default: frame[i] = 0;
+                    1 : case (i)
+                        0 : frame[i][j] = B1;
+                        1 : frame[i][j] = 8'b0;
+                        2 : frame[i][j] = 8'b0;
+                        3 : frame[i][j] = E1;
+                        4 : frame[i][j] = 8'b0;
+                        5 : frame[i][j] = 8'b0;
+                        6 : frame[i][j] = F1;
+                        7 : frame[i][j] = 8'b0;
+                        8 : frame[i][j] = 8'b0;
+                        default: frame[i][j] = 8'b0;
                     endcase
-                    2 : case (row)
-                        0 : frame[i] = 0;
-                        1 : frame[i] = 0;
-                        2 : frame[i] = 0;
-                        3 : frame[i] = 0;
-                        4 : frame[i] = 0;
-                        5 : frame[i] = 0;
-                        6 : frame[i] = 0;
-                        7 : frame[i] = 0;
-                        8 : frame[i] = 0;
-                        default: frame[i] = 0;
+                    2 : case (i)
+                        0 : frame[i][j] = D1;
+                        1 : frame[i][j] = 8'b0;
+                        2 : frame[i][j] = 8'b0;
+                        3 : frame[i][j] = D2;
+                        4 : frame[i][j] = 8'b0;
+                        5 : frame[i][j] = 8'b0;
+                        6 : frame[i][j] = D3;
+                        7 : frame[i][j] = 8'b0;
+                        8 : frame[i][j] = 8'b0;
+                        default: frame[i][j] = 8'b0;
                     endcase
-                    3 : case (row)
-                        0 : frame[i] = 0;
-                        1 : frame[i] = 0;
-                        2 : frame[i] = 0;
-                        3 : frame[i] = 0;
-                        4 : frame[i] = 0;
-                        5 : frame[i] = 0;
-                        6 : frame[i] = 0;
-                        7 : frame[i] = 0;
-                        8 : frame[i] = 0;
-                        default: frame[i] = 0;
+                    3 : case (i)
+                        0 : frame[i][j] = au4.aup[0];
+                        1 : frame[i][j] = 8'b0;
+                        2 : frame[i][j] = 8'b0;
+                        3 : frame[i][j] = au4.aup[1];
+                        4 : frame[i][j] = 8'b0;
+                        5 : frame[i][j] = 8'b0;
+                        6 : frame[i][j] = au4.aup[2];
+                        7 : frame[i][j] = au4.aup[2];
+                        8 : frame[i][j] = au4.aup[2];
+                        default: frame[i][j] = 8'b0;
                     endcase
-                    4 : case (row)
-                        0 : frame[i] = 0;
-                        1 : frame[i] = 0;
-                        2 : frame[i] = 0;
-                        3 : frame[i] = 0;
-                        4 : frame[i] = 0;
-                        5 : frame[i] = 0;
-                        6 : frame[i] = 0;
-                        7 : frame[i] = 0;
-                        8 : frame[i] = 0;
-                        default: frame[i] = 0;
+                    4 : case (i)
+                        0 : frame[i][j] = B2;
+                        1 : frame[i][j] = B2;
+                        2 : frame[i][j] = B2;
+                        3 : frame[i][j] = K1;
+                        4 : frame[i][j] = 8'b0;
+                        5 : frame[i][j] = 8'b0;
+                        6 : frame[i][j] = K2;
+                        7 : frame[i][j] = 8'b0;
+                        8 : frame[i][j] = 8'b0;
+                        default: frame[i][j] = 8'b0;
                     endcase
-                    5 : case (row)
-                        0 : frame[i] = 0;
-                        1 : frame[i] = 0;
-                        2 : frame[i] = 0;
-                        3 : frame[i] = 0;
-                        4 : frame[i] = 0;
-                        5 : frame[i] = 0;
-                        6 : frame[i] = 0;
-                        7 : frame[i] = 0;
-                        8 : frame[i] = 0;
-                        default: frame[i] = 0;
+                    5 : case (i)
+                        0 : frame[i][j] = D4;
+                        1 : frame[i][j] = 8'b0;
+                        2 : frame[i][j] = 8'b0;
+                        3 : frame[i][j] = D5;
+                        4 : frame[i][j] = 8'b0;
+                        5 : frame[i][j] = 8'b0;
+                        6 : frame[i][j] = D6;
+                        7 : frame[i][j] = 8'b0;
+                        8 : frame[i][j] = 8'b0;
+                        default: frame[i][j] = 8'b0;
                     endcase
-                    6 : case (row)
-                        0 : frame[i] = 0;
-                        1 : frame[i] = 0;
-                        2 : frame[i] = 0;
-                        3 : frame[i] = 0;
-                        4 : frame[i] = 0;
-                        5 : frame[i] = 0;
-                        6 : frame[i] = 0;
-                        7 : frame[i] = 0;
-                        8 : frame[i] = 0;
-                        default: frame[i] = 0;
+                    6 : case (i)
+                        0 : frame[i][j] = D7;
+                        1 : frame[i][j] = 8'b0;
+                        2 : frame[i][j] = 8'b0;
+                        3 : frame[i][j] = D8;
+                        4 : frame[i][j] = 8'b0;
+                        5 : frame[i][j] = 8'b0;
+                        6 : frame[i][j] = D9;
+                        7 : frame[i][j] = 8'b0;
+                        8 : frame[i][j] = 8'b0;
+                        default: frame[i][j] = 8'b0;
                     endcase
-                    7 : case (row)
-                        0 : frame[i] = 0;
-                        1 : frame[i] = 0;
-                        2 : frame[i] = 0;
-                        3 : frame[i] = 0;
-                        4 : frame[i] = 0;
-                        5 : frame[i] = 0;
-                        6 : frame[i] = 0;
-                        7 : frame[i] = 0;
-                        8 : frame[i] = 0;
-                        default: frame[i] = 0;
+                    7 : case (i)
+                        0 : frame[i][j] = D10;
+                        1 : frame[i][j] = 8'b0;
+                        2 : frame[i][j] = 8'b0;
+                        3 : frame[i][j] = D11;
+                        4 : frame[i][j] = 8'b0;
+                        5 : frame[i][j] = 8'b0;
+                        6 : frame[i][j] = D12;
+                        7 : frame[i][j] = 8'b0;
+                        8 : frame[i][j] = 8'b0;
+                        default: frame[i][j] = 8'b0;
                     endcase
-                    8 : case (row)
-                        0 : frame[i] = 0;
-                        1 : frame[i] = 0;
-                        2 : frame[i] = 0;
-                        3 : frame[i] = 0;
-                        4 : frame[i] = 0;
-                        5 : frame[i] = 0;
-                        6 : frame[i] = 0;
-                        7 : frame[i] = 0;
-                        8 : frame[i] = 0;
-                        default: frame[i] = 0;
+                    8 : case (i)
+                        0 : frame[i][j] = S1;
+                        1 : frame[i][j] = Z1;
+                        2 : frame[i][j] = Z1;
+                        3 : frame[i][j] = Z2;
+                        4 : frame[i][j] = Z2;
+                        5 : frame[i][j] = M1;
+                        6 : frame[i][j] = E2;
+                        7 : frame[i][j] = 8'b0;
+                        8 : frame[i][j] = 8'b0;
+                        default: frame[i][j] = 8'b0;
                     endcase
-                    default: default: frame[i] = 0;
+                    default: default: frame[i][j] = 0;
                 endcase
             end else begin
-                frame[i] = au4.vc4.c4.data[(row*c4_Lenght)+(col-9)];
+                frame[i][j] = au4.vc4.c4.data[(i*c4_Lenght)+(j-9)];
             end
             frame = this.frame;
-            return frame;
+            
+            end
         end
+        return frame;
     endfunction
 
 
@@ -212,12 +241,12 @@ class STM_1;
             13 : previous_J0_frame[2];
             14 : previous_J0_frame[1];
             15 : previous_J0_frame[0];
-            default: 8'b00000000;
+            default: 8'b0;
         endcase
     endfunction
 
     function bit [7:0] calculate_B1();
-        previous_B1 = 8'b00000000;
+        previous_B1 = 8'b0;
         for (int i = 0; i < STM1_Lenght*STM1_Width*Byte_Num ; i++) begin
             previous_B1 = previous_B1 ^ frame[i];
         end
@@ -226,26 +255,31 @@ class STM_1;
 
     function bit [7:0] calculate_E1();
         // null 0 
+        return 8'b0;
     endfunction
 
     function bit [7:0] calculate_F1();
         // null
+        return 8'b0;
     endfunction
 
     function bit [7:0] calculate_D1();
         // writable
+        return 8'b0;
     endfunction
 
     function bit [7:0] calculate_D2();
         // writable
+        return 8'b0;
     endfunction
 
     function bit [7:0] calculate_D3();
         // writable
+        return 8'b0;
     endfunction
 
     function bit [7:0] calculate_B2();
-        previous_B2 = 24'h000000;
+        previous_B2 = 24'h0;
         for (int i = 0; i < STM1_Lenght*STM1_Width*Byte_Num ; i+=3 ) begin
             previous_B2 = previous_B2 ^ {frame[i],frame[i+1],frame[i+2]};
         end
@@ -254,66 +288,82 @@ class STM_1;
 
     function bit [7:0] calculate_K1();
         // null
+        return 8'b0;
     endfunction
 
     function bit [7:0] calculate_K2();
         // null
+        return 8'b0;
     endfunction
 
     function bit [7:0] calculate_D4();
         // writable
+        return 8'b0;
     endfunction
 
     function bit [7:0] calculate_D5();
         // writable
+        return 8'b0;
     endfunction
 
     function bit [7:0] calculate_D6();
         // writable
+        return 8'b0;
     endfunction
 
     function bit [7:0] calculate_D7();
         // writable
+        return 8'b0;
     endfunction
     
     function bit [7:0] calculate_D8();
         // writable
+        return 8'b0;
     endfunction
     
     function bit [7:0] calculate_D9();
         // writable
+        return 8'b0;
     endfunction
 
     function bit [7:0] calculate_D10();
         // writable
+        return 8'b0;
     endfunction
 
     function bit [7:0] calculate_D11();
         // writable
+        return 8'b0;
     endfunction
 
     function bit [7:0] calculate_D12();
         // writable
+        return 8'b0;
     endfunction
 
     function bit [7:0] calculate_S1();
         // null
+        return 8'b0;
     endfunction
 
     function bit [7:0] calculate_Z1();
         // null
+        return 8'b0;
     endfunction
 
     function bit [7:0] calculate_Z2();
         // null
+        return 8'b0;
     endfunction
 
     function bit [7:0] calculate_M1();
         // null
+        return 8'b0;
     endfunction
 
     function bit [7:0] calculate_E2();
         // null
+        return 8'b0;
     endfunction
 
     function bit [7:0] crc7_sdh_j0 (
