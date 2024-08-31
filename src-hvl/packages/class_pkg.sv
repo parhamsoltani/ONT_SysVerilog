3eeef9a46434714d4f14c50e5043dacf0c26e669
alisoltani8200 <110608793+alisoltani8200@users.noreply.github.com>
2024-08-31 12:00:24 +0330

    Add files via upload

---
+++ src-hvl/packages/class_pkg.sv
@@ -0,0 +1,6 @@
+package class_pkg;
+    `include "C4.sv"
+    `include "VC4.sv"
+    `include "AU4.sv"
+    `include "STM1.sv"
+endpackage
---
+++ src-hvl/packages/param_pkg.sv
@@ -0,0 +1,7 @@
+package param_pkg;
+    parameter c4_Lenght = 260, c4_Width = 9, Byte_Num = 8 ;
+    parameter vc4_Lenght = 261, vc4_Width = 9;
+    parameter STM1_Lenght = 270, STM1_Width = 9;
+    typedef int c4_index_fixed_array2_t[2];
+
+endpackage
