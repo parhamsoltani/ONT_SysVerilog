c94aa670c416b50afc4afa0e888d914c00f97fbb
Taha Memari <t.memari81.b@gmail.com>
2024-09-01 14:57:06 +0330

    Add files via upload

--- src/hvl/packages/param_pkg.sv
+++ src/hvl/packages/param_pkg.sv
@@ -1,23 +1,22 @@
 package param_pkg;
 
     // global params & typedefs 
-
+    typedef int dimensions_t[2];
 
 
     // C4 params & typedefs    
-    parameter c4_Lenght = 260,
+    parameter c4_Length = 260,
               c4_Width = 9, 
               Byte_Num = 8;
 
-    typedef int c4_index_fixed_array2_t[2];
-
-
+    // VC4 params & typedefs    
+    parameter vc4_Length = 260,
+              vc4_Width = 9;
 
     // CSV params & typedefs
     parameter row_based = 0,
-              col_based = 0;
+              col_based = 1;
 
-    typedef int dimensions_t[2];
 
 
 endpackage
