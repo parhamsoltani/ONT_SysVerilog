98835bd3bda9b4e5c91ca9a24012ab393e1b1397
Taha Memari <t.memari81.b@gmail.com>
2024-09-03 10:18:51 +0330

    Add files via upload

--- src/hvl/packages/param_pkg.sv
+++ src/hvl/packages/param_pkg.sv
@@ -13,10 +13,26 @@ package param_pkg;
     parameter vc4_Length = 260,
               vc4_Width = 9;
 
-    // CSV params & typedefs
-    parameter row_based = 0,
-              col_based = 1;
+    // STM1 params & typedefs
+    parameter STM1_Length = 270, STM1_Width = 9; 
+    parameter P_letter = 8'b01010000,
+              A_letter = 8'b01000000,
+              R_letter = 8'b01010010,
+              M_letter = 8'b01001101,
+              N_letter = 8'b01001110,
+              SPACE_letter = 8'b00100000;
 
+    // CSV params & typedefs
+    parameter bit row_based = 0;
+    parameter bit col_based = 1;
+    parameter bit HEX = 0;
+    parameter bit DEC = 1;
+    parameter bit TRANSLATE_OFF = 0;
+    parameter bit TRANSLATE_ON = 1;
+
+
+
+    
 
 
 endpackage
