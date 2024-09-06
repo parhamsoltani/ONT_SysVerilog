c6cc02e71f4b6cfb574b3f3b3da6e211c2a83bd4
Taha Memari <t.memari81.b@gmail.com>
2024-09-06 10:59:28 +0330

    Update C4.sv

--- src/hvl/classes/C4.sv
+++ src/hvl/classes/C4.sv
@@ -3,19 +3,17 @@ import class_pkg::*;
 
 
 class C4;
-    rand bit [c4_Width-1:0][c4_Length-1:0][Byte_Num-1:0] data;
+    rand bit [0:C4_WIDTH-1][0:C4_LENGTH-1][BYTE_NUM-1:0] data;
 
     function void pre_randomize ();
-        $display ("C4: This will be called just before randomization");
+        // $display ("C4: This will be called just before randomization");
     endfunction
 
     function void post_randomize ();
-        $display ("C4: This will be called just after randomization");
+        // $display ("C4: This will be called just after randomization");
     endfunction
 
     function int index_2Dto1D(input int row, column);
-        return row * c4_Width + column;
+        return row * C4_WIDTH + column;
     endfunction   
 endclass
-
-
