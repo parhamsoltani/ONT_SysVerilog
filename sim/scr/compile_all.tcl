bcd47ca73f977f607cadc7a3b85ef7052ea8a948
Taha Memari <t.memari81.b@gmail.com>
2024-09-06 10:57:17 +0330

    Update compile_all.tcl
    Root Address Updated

--- sim/scr/compile_all.tcl
+++ sim/scr/compile_all.tcl
@@ -1,4 +1,4 @@
     vlog -vopt -sv $project_path/src/hvl/packages/param_pkg.sv
-    vlog -vopt -sv $project_path/src/hvl/classes/class_pkg.sv
+    vlog -vopt -sv $project_path/src/hvl/packages/class_pkg.sv
 
     vlog -vopt -sv $project_path/sim/tb/tb1.sv
