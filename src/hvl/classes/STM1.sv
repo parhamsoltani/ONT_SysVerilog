7e4ffe47c71f48ee9e23a68fa40ff238e64e1dc8
Taha Memari <t.memari81.b@gmail.com>
2024-09-06 11:02:36 +0330

    Update STM1.sv
    Cleaning the code + fixing update_J0_trace_message()

--- src/hvl/classes/STM1.sv
+++ src/hvl/classes/STM1.sv
@@ -2,9 +2,9 @@ import param_pkg::*;
 import class_pkg::*;
 
 class STM1;
-    AU4 au4;
+    rand AU4 au4;
     // STM-1 full frame
-    bit [STM1_Width-1:0][STM1_Length-1:0][Byte_Num-1:0] frame;
+    bit [0:STM1_WIDTH-1][0:STM1_LENGTH-1][BYTE_NUM-1:0] frame;
     // RSOH
     bit [7:0] A1;
     bit [7:0] A2;
@@ -35,19 +35,23 @@ class STM1;
     bit [7:0] M1;
     bit [7:0] E2;
 
+    // AUP
+    bit [7:0] H1;
+    bit [7:0] H2;
+    bit [7:0] H3;
+
     // extra variables for xor calculation for B1 and B2
     bit[7:0] frame_xor;
     bit [23:0] semi_frame_xor;
     
     // J0 parametere
     localparam int J0_FRAME_LENGTH = 16;
-    string J0_TRACE_MESSAGE = "PARMAN";
-    static byte  j0_frame [0:J0_FRAME_LENGTH-1];
+    static string J0_TRACE_MESSAGE = "PARMAN";
+    static bit [0:J0_FRAME_LENGTH-1][7:0] j0_frame ;
     static bit [3:0] j0_frame_counter = 0;
 
-    rand bit [8:0][269:0][7:0] f;
 
-    function new(bit[7:0] vc4_xor = 0, bit[7:0] frame_xor = 0, bit [23:0] semi_frame_xor = 0);
+    function new(bit[7:0] vc4_xor = 0, bit[7:0] frame_xor = 0, bit[23:0] semi_frame_xor = 0);
         au4 = new(vc4_xor); 
         init_j0_frame();
         B1 = frame_xor;
@@ -55,17 +59,13 @@ class STM1;
     endfunction
 
     function void pre_randomize();
-        if (!au4.randomize()) begin
-            $display("STM-1: AU4 randomization failed");
-        end else begin
-            $display("STM-1: This will be called just before randomization");
-            calculate_soh();
-        end
+        // $display("STM-1: This will be called just before randomization");
+        
     endfunction
 
     function void post_randomize();
-        $display("STM-1: This will be called just after randomization");
-        
+        // $display("STM-1: This will be called just after randomization");
+        calculate_soh();
     endfunction
 
     function void calculate_soh();
@@ -102,130 +102,45 @@ class STM1;
         calculate_semi_frame_xor();     
     endfunction
     
-    function void  update_stm_frame();
-        for (int i = 0; i < STM1_Width ; i++) begin
-            for (int j = 0; j < STM1_Length ; j++) begin
-                if(j < 9) begin
-                    case (i)
-                        0 : case (j)
-                                0 : frame[i][j] = A1;
-                                1 : frame[i][j] = A1;
-                                2 : frame[i][j] = A1;
-                                3 : frame[i][j] = A2;
-                                4 : frame[i][j] = A2;
-                                5 : frame[i][j] = A2;
-                                6 : frame[i][j] = J0;
-                                7 : frame[i][j] = 8'b0;
-                                8 : frame[i][j] = 8'b0;
-                                default: frame[i][j] = 8'b0;
-                            endcase
-                        1 : case (j)
-                                0 : frame[i][j] = B1;
-                                1 : frame[i][j] = 8'b0;
-                                2 : frame[i][j] = 8'b0;
-                                3 : frame[i][j] = E1;
-                                4 : frame[i][j] = 8'b0;
-                                5 : frame[i][j] = 8'b0;
-                                6 : frame[i][j] = F1;
-                                7 : frame[i][j] = 8'b0;
-                                8 : frame[i][j] = 8'b0;
-                                default: frame[i][j] = 8'b0;
-                            endcase
-                        2 : case (j)
-                                0 : frame[i][j] = D1;
-                                1 : frame[i][j] = 8'b0;
-                                2 : frame[i][j] = 8'b0;
-                                3 : frame[i][j] = D2;
-                                4 : frame[i][j] = 8'b0;
-                                5 : frame[i][j] = 8'b0;
-                                6 : frame[i][j] = D3;
-                                7 : frame[i][j] = 8'b0;
-                                8 : frame[i][j] = 8'b0;
-                                default: frame[i][j] = 8'b0;
-                            endcase
-                        3 : case (j)
-                                0 : frame[i][j] = au4.aup[0];
-                                1 : frame[i][j] = 8'b0;
-                                2 : frame[i][j] = 8'b0;
-                                3 : frame[i][j] = au4.aup[1];
-                                4 : frame[i][j] = 8'b0;
-                                5 : frame[i][j] = 8'b0;
-                                6 : frame[i][j] = au4.aup[2];
-                                7 : frame[i][j] = au4.aup[2];
-                                8 : frame[i][j] = au4.aup[2];
-                                default: frame[i][j] = 8'b0;
-                            endcase
-                        4 : case (j)
-                                0 : frame[i][j] = B2[23:16];
-                                1 : frame[i][j] = B2[15:8];
-                                2 : frame[i][j] = B2[7:0];
-                                3 : frame[i][j] = K1;
-                                4 : frame[i][j] = 8'b0;
-                                5 : frame[i][j] = 8'b0;
-                                6 : frame[i][j] = K2;
-                                7 : frame[i][j] = 8'b0;
-                                8 : frame[i][j] = 8'b0;
-                                default: frame[i][j] = 8'b0;
-                            endcase
-                        5 : case (j)
-                                0 : frame[i][j] = D4;
-                                1 : frame[i][j] = 8'b0;
-                                2 : frame[i][j] = 8'b0;
-                                3 : frame[i][j] = D5;
-                                4 : frame[i][j] = 8'b0;
-                                5 : frame[i][j] = 8'b0;
-                                6 : frame[i][j] = D6;
-                                7 : frame[i][j] = 8'b0;
-                                8 : frame[i][j] = 8'b0;
-                                default: frame[i][j] = 8'b0;
-                            endcase
-                        6 : case (j)
-                                0 : frame[i][j] = D7;
-                                1 : frame[i][j] = 8'b0;
-                                2 : frame[i][j] = 8'b0;
-                                3 : frame[i][j] = D8;
-                                4 : frame[i][j] = 8'b0;
-                                5 : frame[i][j] = 8'b0;
-                                6 : frame[i][j] = D9;
-                                7 : frame[i][j] = 8'b0;
-                                8 : frame[i][j] = 8'b0;
-                                default: frame[i][j] = 8'b0;
-                            endcase
-                        7 : case (j)
-                                0 : frame[i][j] = D10;
-                                1 : frame[i][j] = 8'b0;
-                                2 : frame[i][j] = 8'b0;
-                                3 : frame[i][j] = D11;
-                                4 : frame[i][j] = 8'b0;
-                                5 : frame[i][j] = 8'b0;
-                                6 : frame[i][j] = D12;
-                                7 : frame[i][j] = 8'b0;
-                                8 : frame[i][j] = 8'b0;
-                                default: frame[i][j] = 8'b0;
-                            endcase
-                        8 : case (j)
-                                0 : frame[i][j] = S1;
-                                1 : frame[i][j] = Z1;
-                                2 : frame[i][j] = Z1;
-                                3 : frame[i][j] = Z2;
-                                4 : frame[i][j] = Z2;
-                                5 : frame[i][j] = M1;
-                                6 : frame[i][j] = E2;
-                                7 : frame[i][j] = 8'b0;
-                                8 : frame[i][j] = 8'b0;
-                                default: frame[i][j] = 8'b0;
-                            endcase 
-                        default: frame[i][j] = 8'b0;
-                    endcase
-                end else begin
-                    frame[i][j] = au4.vc4.data[i][j-9];
-                end
+
+    function void update_stm_frame();
+        H1 = au4.aup[0];
+        H2 = au4.aup[1];
+        H3 = au4.aup[2];
+
+        frame[0][0:8] = {    A1     ,    A1    ,  A1     ,  A2  ,   A2   ,    A2   ,  J0  ,  8'b0  ,  8'b0 };
+        frame[1][0:8] = {    B1     ,   8'b0   ,  8'b0   ,  E1  ,  8'b0  ,   8'b0  ,  F1  ,  8'b0  ,  8'b0 };
+        frame[2][0:8] = {    D1     ,   8'b0   ,  8'b0   ,  D2  ,  8'b0  ,   8'b0  ,  D3  ,  8'b0  ,  8'b0 };
+        frame[3][0:8] = {    H1     ,   8'b0   ,  8'b0   ,  H2  ,  8'b0  ,   8'b0  ,  H3  ,   H3   ,   H3  };
+        frame[4][0:8] = { B2[23:16] , B2[15:8] , B2[7:0] ,  K1  ,  8'b0  ,   8'b0  ,  K2  ,  8'b0  ,  8'b0 };
+        frame[5][0:8] = {    D4     ,   8'b0   ,  8'b0   ,  D5  ,  8'b0  ,   8'b0  ,  D6  ,  8'b0  ,  8'b0 };
+        frame[6][0:8] = {    D1     ,   8'b0   ,  8'b0   ,  D2  ,  8'b0  ,   8'b0  ,  D9  ,  8'b0  ,  8'b0 };
+        frame[7][0:8] = {    D10    ,   8'b0   ,  8'b0   ,  D11 ,  8'b0  ,   8'b0  ,  D12 ,  8'b0  ,  8'b0 };
+        frame[8][0:8] = {    S1     ,    Z1    ,  Z1     ,  Z2  ,   Z2   ,    M1   ,  E2  ,  8'b0  ,  8'b0 };
+
+
+        for (int i = 0; i < STM1_WIDTH ; i++) begin
+            for (int j = 9; j < STM1_LENGTH ; j++) begin
+                frame[i][j] = au4.vc4.data[i][j-9];
             end
-            calculate_frame_xor();
-            calculate_semi_frame_xor();
         end
+        calculate_frame_xor();
+        calculate_semi_frame_xor();
     endfunction
 
+    function void update_overheads();
+        frame[0][0:8] = {    A1     ,    A1    ,  A1     ,  A2  ,   A2   ,    A2   ,  J0  ,  8'b0  ,  8'b0 };
+        frame[1][0:8] = {    B1     ,   8'b0   ,  8'b0   ,  E1  ,  8'b0  ,   8'b0  ,  F1  ,  8'b0  ,  8'b0 };
+        frame[2][0:8] = {    D1     ,   8'b0   ,  8'b0   ,  D2  ,  8'b0  ,   8'b0  ,  D3  ,  8'b0  ,  8'b0 };
+        frame[3][0:8] = {    H1     ,   8'b0   ,  8'b0   ,  H2  ,  8'b0  ,   8'b0  ,  H3  ,   H3   ,   H3  };
+        frame[4][0:8] = { B2[23:16] , B2[15:8] , B2[7:0] ,  K1  ,  8'b0  ,   8'b0  ,  K2  ,  8'b0  ,  8'b0 };
+        frame[5][0:8] = {    D4     ,   8'b0   ,  8'b0   ,  D5  ,  8'b0  ,   8'b0  ,  D6  ,  8'b0  ,  8'b0 };
+        frame[6][0:8] = {    D1     ,   8'b0   ,  8'b0   ,  D2  ,  8'b0  ,   8'b0  ,  D9  ,  8'b0  ,  8'b0 };
+        frame[7][0:8] = {    D10    ,   8'b0   ,  8'b0   ,  D11 ,  8'b0  ,   8'b0  ,  D12 ,  8'b0  ,  8'b0 };
+        frame[8][0:8] = {    S1     ,    Z1    ,  Z1     ,  Z2  ,   Z2   ,    M1   ,  E2  ,  8'b0  ,  8'b0 };
+        calculate_frame_xor();
+        calculate_semi_frame_xor();
+    endfunction
 
     function bit [7:0] calculate_A1();
         return 8'hF6;
@@ -357,7 +272,7 @@ class STM1;
     endfunction
 
     function bit [6:0] crc7_calculate (
-        input byte data [0:J0_FRAME_LENGTH-2]  // 15-byte array input
+        input bit [0:J0_FRAME_LENGTH-2][7:0] data  // 15-byte array input
     );
         bit [7:0] crc;
         bit [7:0] byte1;
@@ -390,25 +305,29 @@ class STM1;
         for (int i = 1; i < J0_FRAME_LENGTH; i++) begin
             if (i - 1 < J0_TRACE_MESSAGE.len()) begin
                 j0_frame[i][7] = 1'b0;
-                j0_frame[i][6:0] = byte'(J0_TRACE_MESSAGE[i - 1]);
+                j0_frame[i][6:0] = int'(J0_TRACE_MESSAGE[i - 1]);
             end else begin
                 j0_frame[i][7] = 1'b0;
-                j0_frame[i][6:0] = byte'(" "); // Space character
+                j0_frame[i][6:0] = int'(" "); // Space character
             end
         end
         j0_frame[0][7] = 1'b1;
         j0_frame[0][6:0] = crc7_calculate(j0_frame[1:J0_FRAME_LENGTH-1]);
+        
     endfunction
 
     function void update_J0_trace_message(string new_message);
         J0_TRACE_MESSAGE = new_message;
         init_j0_frame();
+        j0_frame_counter = 0;
+        J0 = calculate_J0();
+        update_overheads();
     endfunction
 
     function void calculate_frame_xor();
         bit[7:0] current_frame_xor = 8'h00;
-        for (int i = 0; i < STM1_Width ; i++) begin
-            for (int j = 0; j < STM1_Length ; j++) begin
+        for (int i = 0; i < STM1_WIDTH ; i++) begin
+            for (int j = 0; j < STM1_LENGTH ; j++) begin
                 current_frame_xor ^= frame[i][j];
             end
         end
@@ -418,8 +337,8 @@ class STM1;
 
     function void calculate_semi_frame_xor();
         bit[23:0] current_semi_frame_xor = 24'h0;
-        for (int i = 0; i < STM1_Width ; i++) begin
-            for (int j = 0; j < STM1_Length ; j+=3) begin
+        for (int i = 0; i < STM1_WIDTH ; i++) begin
+            for (int j = 0; j < STM1_LENGTH ; j+=3) begin
                 current_semi_frame_xor ^= {frame[i][j],frame[i][j+1],frame[i][j+2]};
             end
         end
