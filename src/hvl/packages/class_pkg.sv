45d58163229f0cc61964b4ca2668533ef4a02bea
Taha Memari <t.memari81.b@gmail.com>
2024-09-06 11:06:52 +0330

    Update class_pkg.sv
    Updating the address of classes to the filing standard

--- src/hvl/packages/class_pkg.sv
+++ src/hvl/packages/class_pkg.sv
@@ -1,7 +1,8 @@
 package class_pkg;
-    `include "C4.sv"
-    `include "VC4.sv"
-    `include "AU4.sv"
-    `include "CSV.sv"
-    `include "STM1.sv"
+    `include "..\\classes\\C4.sv"
+    `include "..\\classes\\VC4.sv"
+    `include "..\\classes\\AU4.sv"
+    `include "..\\classes\\CSV.sv"
+    `include "..\\classes\\STM1.sv"
 endpackage
+    
