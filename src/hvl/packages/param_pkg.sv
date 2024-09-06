f4366d3d1a7db6678bed140294fd85367dcbe891
Taha Memari <t.memari81.b@gmail.com>
2024-09-06 11:05:36 +0330

    Update param_pkg.sv
    Writing the parameters in Capital

--- src/hvl/packages/param_pkg.sv
+++ src/hvl/packages/param_pkg.sv
@@ -1,32 +1,29 @@
 package param_pkg;
 
     // global params & typedefs 
-    typedef bit [0:1][31:0] dimensions_t;
+    typedef bit[0:1][31:0] dimensions_t;
 
 
     // C4 params & typedefs    
-    parameter c4_Length = 260,
-              c4_Width = 9, 
-              Byte_Num = 8;
+    parameter int C4_LENGTH = 260;
+    parameter int C4_WIDTH = 9; 
+    parameter int BYTE_NUM = 8;
 
     // VC4 params & typedefs    
-    parameter vc4_Length = 261,
-              vc4_Width = 9;
+    parameter int VC4_LENGTH = 261;
+    parameter int VC4_WIDTH = 9;
 
     // STM1 params & typedefs
-    parameter STM1_Length = 270, STM1_Width = 9; 
+    parameter int STM1_LENGTH = 270;
+    parameter int STM1_WIDTH = 9; 
 
-    // CSV params & typedefs
+    // CSV params & typedefs   
     parameter byte C4_OUT = 0;
     parameter byte VC4_OUT = 1;   
-    parameter byte STM1_OUT = 2; 
-    
-    parameter bit row_based = 0;
-    parameter bit col_based = 1;
+    parameter byte STM1_OUT = 2;  
+
+    parameter bit ROW_BASED = 0;
+    parameter bit COL_BASED = 1;
     parameter bit HEX = 0;
     parameter bit DEC = 1;
-    parameter bit TRANSLATE_OFF = 0;
-    parameter bit TRANSLATE_ON = 1;    
-
-
 endpackage
