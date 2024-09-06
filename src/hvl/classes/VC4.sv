505245e7cbc5477ca65d62e2681e46c993b77fb7
Taha Memari <t.memari81.b@gmail.com>
2024-09-06 11:00:50 +0330

    Update VC4.sv
    Cleaning the code + Added C2 & J1 adjustment function at the current frame

--- src/hvl/classes/VC4.sv
+++ src/hvl/classes/VC4.sv
@@ -2,20 +2,30 @@ import param_pkg::*;
 import class_pkg::*;
 
 class VC4;
-    C4 c4;
-    rand bit [Byte_Num-1:0] poh[c4_Width]; // Path Overhead
-    bit [vc4_Width-1:0][vc4_Length-1:0][Byte_Num-1:0] data;
-    bit[7:0] J1, B3, C2, G1, F2, H4, F3, K3, N1;
+    rand C4 c4;
+    rand bit [0:C4_WIDTH-1][BYTE_NUM-1:0] poh; // Path Overhead
+    bit [0:VC4_WIDTH-1][0:VC4_LENGTH-1][BYTE_NUM-1:0] data;
+    bit [7:0] J1; 
+    bit [7:0] B3;
+    bit [7:0] C2;
+    bit [7:0] G1;
+    bit [7:0] F2;
+    bit [7:0] H4;
+    bit [7:0] F3;
+    bit [7:0] K3;
+    bit [7:0] N1;
 
     // J1 trace variables
     localparam int J1_FRAME_LENGTH = 16;
-    string J1_TRACE_MESSAGE = "PARMAN";
-    static byte j1_frame[0:J1_FRAME_LENGTH-1];
-    static int j1_frame_counter = 0;
+    static string J1_TRACE_MESSAGE = "PARMAN";
+    static bit[0:J1_FRAME_LENGTH-1][7:0] j1_frame;
+    static bit[3:0] j1_frame_counter = 0;
 
     // B3 and C2 related variables
     bit[7:0] vc4_xor;
-    bit[7:0] c2_value;
+    
+
+    /*
     typedef enum bit [7:0] {
         UNEQUIPPED           = 8'h00,
         EQUIPPED_NON_SPECIFIC = 8'h01,
@@ -27,7 +37,7 @@ class VC4;
         MAN_DQDB_MAPPING     = 8'h14,
         FDDI_MAPPING         = 8'h15
     } c2_state_e;
-
+    */
 
 
     function new(bit[7:0] vc4_xor = 0);
@@ -38,11 +48,15 @@ class VC4;
 
 
     function void pre_randomize();
-        if (!c4.randomize()) begin
-            $display("VC4: C4 randomization failed");
-        end else begin
-            $display("VC4: This will be called just before randomization");
-            // Initialize Path Overhead according to ITU-T G.707
+        // $display("VC4: This will be called just before randomization");
+        // Initialize Path Overhead according to ITU-T G.707
+         
+        
+        
+    endfunction
+
+    function void post_randomize();
+        // $display("VC4: This will be called just after randomization");
             J1 = calculate_J1();
             B3 = calculate_B3();
             C2 = calculate_C2();
@@ -52,29 +66,13 @@ class VC4;
             F3 = calculate_F3();
             K3 = calculate_K3();
             N1 = calculate_N1();
-
-            // Update POH array
-            poh[0] = J1;
-            poh[1] = B3;
-            poh[2] = C2;
-            poh[3] = G1;
-            poh[4] = F2;
-            poh[5] = H4;
-            poh[6] = F3;
-            poh[7] = K3;
-            poh[8] = N1;
-            insert_poh(); 
-            calculate_vc4_xor();
-        end
-    endfunction
-
-    function void post_randomize();
-        $display("VC4: This will be called just after randomization");
+            insert_data();
+            insert_poh();
     endfunction
 
     function void  update_vc4_data();
-        for (int i = 0; i < vc4_Width ; i++) begin
-            for (int j = 0; j < vc4_Length ; j++) begin
+        for (int i = 0; i < VC4_WIDTH ; i++) begin
+            for (int j = 0; j < VC4_LENGTH ; j++) begin
                 if(j == 0) begin
                     case (i)
                         0 : data[i][j] = J1;
@@ -106,8 +104,7 @@ class VC4;
     endfunction
 
     function bit [7:0] calculate_C2();
-        c2_value = 8'h00;
-        return c2_value;
+        return C2;
     endfunction
 
     function bit [7:0] calculate_G1();
@@ -115,8 +112,8 @@ class VC4;
         // Bits 1-4: REI (Remote Error Indication)
         // Bits 5-7: Path Status
         // Bit 8: Reserved
-        byte rei = 4'h0; // Assuming no errors
-        byte path_status = 3'b000; // Assuming normal operation
+        bit[3:0] rei = 4'h0; // Assuming no errors
+        bit[3:0] path_status = 3'b000; // Assuming normal operation
         return {1'b0, path_status, rei};
     endfunction
 
@@ -131,8 +128,8 @@ class VC4;
         // H4: Multiframe Indicator
         // Used for payloads requiring multiframe alignment
         // For this application, we'll use a simple counter
-        static byte h4_counter = 0;
-        h4_counter = (h4_counter + 1) % 16; // 4-bit counter
+        static bit[3:0] h4_counter = 0;    // 4-bit counter
+        h4_counter = (h4_counter + 1);   
         return h4_counter;
     endfunction
 
@@ -163,10 +160,10 @@ class VC4;
         for (int i = 1; i < J1_FRAME_LENGTH; i++) begin
             if (i - 1 < J1_TRACE_MESSAGE.len()) begin
                 j1_frame[i][7] = 1'b0;
-                j1_frame[i][6:0] = byte'(J1_TRACE_MESSAGE[i - 1]);
+                j1_frame[i][6:0] = int'(J1_TRACE_MESSAGE[i - 1]);
             end else begin
                 j1_frame[i][7] = 1'b0;
-                j1_frame[i][6:0] = byte'(" "); // Space character
+                j1_frame[i][6:0] = int'(" "); // Space character
             end
         end
         j1_frame[0][7] = 1'b1;
@@ -176,11 +173,14 @@ class VC4;
     // New function to update J1 trace message
     // Usage:     vc4_instance.update_j1_trace_message("NEW_MESSAGE____");
     function void update_j1_trace_message(string new_message);
-       J1_TRACE_MESSAGE = new_message;
-       init_j1_frame();
+        J1_TRACE_MESSAGE = new_message;
+        init_j1_frame();
+        j1_frame_counter = 0;
+        J1 = calculate_J1();
+        insert_poh();
     endfunction
 
-    function bit [6:0] crc7_calculate (input byte data [0:J1_FRAME_LENGTH-2]);
+    function bit [6:0] crc7_calculate (input bit [0:J1_FRAME_LENGTH-2][7:0] data );
         bit [7:0] crc;
         bit [7:0] byte1;
         integer i, j;
@@ -209,8 +209,8 @@ class VC4;
 
     function void calculate_vc4_xor();
         bit[7:0] current_vc4_xor = 8'h00;
-        for (int i = 0; i < vc4_Width ; i++) begin
-            for (int j = 0; j < vc4_Length ; j++) begin
+        for (int i = 0; i < VC4_WIDTH ; i++) begin
+            for (int j = 0; j < VC4_LENGTH ; j++) begin
                 current_vc4_xor ^= data[i][j];
             end
         end
@@ -219,17 +219,14 @@ class VC4;
 
     // Function to set C2 value
     function void set_c2_value(bit[7:0] value);
-        c2_value = value;
         C2 = value;
-        poh[2] = C2;
-        data[2][0] = C2;
-        update_vc4_data();
-        display_c2_state(value);
+        insert_poh();
+        // display_c2_state(value);
     endfunction
 
     // Function to get C2 state as a string based on the current c2_value
     function string get_c2_state_string();
-        case (c2_value)
+        case (C2)
             8'h00: return "Unequipped";
             8'h01: return "Equipped - non specific";
             8'h02: return "TUG structure";
@@ -254,23 +251,38 @@ class VC4;
     function void insert_poh();
         // Create a new array with increased width
         // Add POH as the last column
+
+        // Update POH array
+        poh[0] = J1;
+        poh[1] = B3;
+        poh[2] = C2;
+        poh[3] = G1;
+        poh[4] = F2;
+        poh[5] = H4;
+        poh[6] = F3;
+        poh[7] = K3;
+        poh[8] = N1;
+
         for (int i = 0; i < 9; i++) begin
             data[i][0] = poh[i];
         end
-        
 
-        // Copy existing data
-        for (int i = 0; i < c4_Width; i++) begin
-            for (int j = 1; j < 261; j++) begin
-                data[i][j] = c4.data[i][j-1];
-            end
-        end
+        calculate_vc4_xor();
     endfunction
 
     function void display_poh();
         $display("VC4: Path Overhead");
         for (int i = 0; i < 9; i++) begin
             $display("POH[%0d] = 0x%0h", i, poh[i]);
+        end
+    endfunction
+
+    function void insert_data();
+        // Copy existing data
+        for (int i = 0; i < VC4_WIDTH; i++) begin
+            for (int j = 1; j < VC4_LENGTH; j++) begin
+                data[i][j] = c4.data[i][j-1];
+            end
         end
     endfunction
 
